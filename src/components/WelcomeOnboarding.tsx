cbd4f0bb55481bd55302a4ee3460250083d1addc
adrija <adrija.kalikaite3@gmail.com>
2026-09-21 15:13:35 +0300

    Prepare language access for important update popup

--- src/components/WelcomeOnboarding.tsx
+++ src/components/WelcomeOnboarding.tsx
@@ -10,6 +10,7 @@ import { Input } from "@/components/ui/input";
 import { Dialog, DialogContent, DialogHeader, DialogTitle } from "@/components/ui/dialog";
 import { toast } from "sonner";
 import { cn } from "@/lib/utils";
+import { useLanguage } from "@/contexts/LanguageContext";
 import { enablePushNotifications, getPushSubscription, pushNotificationsSupported } from "@/lib/pushNotifications";
 
 
